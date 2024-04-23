6e85d2ab7469698572d127f4ce6df156c28215b4
std3 <67806187+standard3@users.noreply.github.com>
2024-04-23 21:35:19 +0200

    docs: documented exported modules

---
+++ mmushell/architectures/__init__.py
@@ -0,0 +1,13 @@
+"""
+This package contains the architecture-specific modules used by the `mmushell` script.
+
+Modules exported by this package:
+
+- `generic`: Architecture-agnostic generic implementation for CPU and memory management related functions and classes.
+- `aarch64`: AArch64-specific functions and classes.
+- `arm`: ARM-specific functions and classes.
+- `intel`: Intel-specific functions and classes.
+- `mips`: MIPS-specific functions and classes.
+- `ppc`: PowerPC-specific functions and classes.
+- `riscv`: RISC-V-specific functions and classes.
+"""
